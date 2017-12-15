b941b31f92c946464001b02f27f36a9d732298e0
AhmedDweidar <32782785+AhmedDweidar@users.noreply.github.com>
2017-12-15 14:14:08 -0500

    added files to BinarySearch folder

---
+++ BinarySearch/BinarySearch.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    public class BinarySearch
+    {
+        public int DoBinarySearch(int[] arr, int target)
+        {
+            int min = 0;
+            int max = arr.Length - 1;
+
+            // must sort array before performing binary search
+            Array.Sort(arr);
+
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+
+                if (arr[mid] == target)
+                {
+                    return mid;
+                }
+
+                if (target > arr[mid])
+                {
+                    min = mid + 1;
+                }
+                else
+                {
+                    max = mid - 1;
+                }
+            }
+
+            return -1;
+        }
+
+        public void Print(int[] arr)
+        {
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ BinarySearch/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 7, 56, 48, 3, 25, 17 };
+            BinarySearch bs = new BinarySearch();
+            var result = bs.DoBinarySearch(numbers, 48);
+            bs.Print(numbers);
+            Console.WriteLine(result);
+
+            Console.Write("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
